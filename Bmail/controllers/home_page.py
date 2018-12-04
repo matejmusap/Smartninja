99494ccab12b49801a76bec8183946bc80771c54
matejmusap <matej.musap@gmail.com>
2018-12-04 11:03:59 +0100

    message-view

--- Bmail/controllers/home_page.py
+++ Bmail/controllers/home_page.py
@@ -1,5 +1,7 @@
 from base import BaseHandler
+from models.message import Message
 
 class HomePageHandler(BaseHandler):
     def get(self, nickname=None):
-        return self.render_template("home_page.html", params={})
+        message = Message.query().order(Message.created)
+        return self.render_template("home_page.html", params={"message": message})
